9848290be6b8fc13d2cb1b881e20d25fd008e490
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-10-04 18:43:48 +0900

    Initial Commit

---
+++ 2009odai1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2009odai1
+//
+//  Created by ビザンコムマック０７ on 2014/10/04.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2009odai1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2009odai1
+//
+//  Created by ビザンコムマック０７ on 2014/10/04.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
